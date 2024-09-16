203f18f03778f2b5b9297db94f3370cd865e2617
KilqGo <nightmax13@mail.ru>
2024-09-16 18:35:53 +0300

    321

--- rooms.py
+++ rooms.py
@@ -1,28 +1,27 @@
 class Room:
-    def __init__(self, room_text, battle_id, connected_rooms=None):
+    def __init__(self, room_text, connected_rooms=None):
         self.room_text = room_text
-        self.battle_id = battle_id
         self.connected_rooms = connected_rooms if connected_rooms is not None else []
 
     def __str__(self):
-        return f"{self.room_text} (Battle ID: {self.battle_id})"
+        return f"{self.room_text} "
 
 
 def main():
 
     rooms = [
-        Room("Начало", 0, ["Верх 1", "Низ 1"]),
-        Room("Верх 1", 1, ["Верх 2"]),
-        Room("Верх 2", 2, ["Конец"]),
-        Room("Низ 1", 1, ["Низ 2"]),
-        Room("Низ 2", 0, ["Конец"]),
-        Room("Конец", 0)
+        Room("Начало",  ["Верх 1", "Низ 1"]),
+        Room("Верх 1",  ["Верх 2"]),
+        Room("Верх 2",  ["Конец"]),
+        Room("Низ 1",  ["Низ 2"]),
+        Room("Низ 2",  ["Конец"]),
+        Room("Конец")
     ]
 
     current_room = rooms[0]  # начало
 
     while True:
-        if current_room.room_text == "Конец":
+        if not current_room.connected_rooms:
             print("Победа!")
             break
 
